b359a4c9c55b3548d972345a69451b2b22c025c9
dengpeng <1092518267@qq.com>
2023-07-26 17:23:21 +0800

    接入SaToken

--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -1,9 +1,16 @@
 package com.example.springbootdemo;
 
+import cn.dev33.satoken.SaManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+/**
+ * 启动类
+ *
+ * @author banyear
+ * @date 2023/07/26
+ */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringBootDemoApplication {
 
--- src/main/java/com/example/springbootdemo/controller/HelloWord.java
+++ src/main/java/com/example/springbootdemo/controller/HelloWord.java
@@ -1,5 +1,6 @@
 package com.example.springbootdemo.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,15 @@ public class HelloWord {
 
     @RequestMapping("/word")
     public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping("/login")
+    public String login(String username, String password) {
+        if ("admin".equals(username)) {
+            StpUtil.login("admin");
+            System.out.println("登录成功");
+        }
         return "hello";
     }
 }
